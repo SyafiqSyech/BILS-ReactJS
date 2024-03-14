233007baf5c7ded1cbf0963190f4502210194d2c
SyafiqSyech <106647971+SyafiqSyech@users.noreply.github.com>
2024-03-14 16:45:05 +0700

    added stuff

--- src/sections/Gofood.tsx
+++ src/sections/Gofood.tsx
@@ -38,38 +38,43 @@ const Gofood = () => {
   }
 
   return (
-    <div className='flex justify-between px-16 py-16 pt-64 max-md:px-8'>
-      <div className='w-1/2 pr-16 flex flex-col justify-between gap-16'>
+    <div className='grid grid-cols-2 max-md:flex max-md:flex-col px-16 py-16 pt-64 max-md:px-8'>
+      <div className='md:pr-16 flex flex-col justify-center gap-16 max-lg:gap-12 max-md:gap-8'>
         {/* <p className='w-96'>
           Who was the first person to think making coffee from an animal's poop would be a great idea? That person is freaky if I do say so myself. And now that kind of coffee is one of the most expensive kind. The world <span className='italic text-[#ad7953]'>brews</span> in strange ways.
         </p> */}
         {/* <p className='txt2'>Lorem Ipsum Dolor</p> */}
-        <p className='Libre text-9xl pb-8'>Sip and savor our <span className='italic text-[#ad7953]'>coffee</span> on the go.</p>
-        <div className='flex items-center gap-2'>
-          <p className='txt1'>order on</p>
+        <p className='Libre text-9xl max-xl:text-8xl max-lg:text-7xl max-md:text-8xl max-sm:text-7xl'>Sip and savor our <span className='italic text-[#ad7953]'>coffee</span> on the&nbsp;go.</p>
+        <div className='flex items-center gap-2 max-md:hidden'>
+          <p className='txt1'>make a</p>
           <Button text='Go-Food' onClick={ undefined }/>
-          <p className='txt1'>now !</p>
+          <p className='txt1'>order ?</p>
         </div>
       </div>
       <motion.div
         initial='rest'
         whileHover='hover'
         animate='rest'
-        className='w-1/2 relative' 
+        className='relative max-md:pt-8 flex justify-end' 
       >
         <motion.p
           variants={ textState }
-          className='absolute !bg-transparent text-center w-full -bottom-14 z-[1]'
+          className='absolute !bg-transparent text-center w-full -bottom-14 z-[1] max-md:hidden'
         >
           Take out and take off!
         </motion.p>
         <motion.img 
           variants={ imgState }
-          className='rounded-[2rem] shad object-right object-cover relative z-[2]' 
+          className='rounded-[2rem] max-h-[50rem] max-md:max-h-[20rem] h-full max-md:w-3/4 shad object-right object-cover relative z-[2]' 
           src='Pilot.jpg' 
           alt="pilot"
         />
       </motion.div>
+      <div className='flex items-center gap-2 w-full pt-10 md:hidden'>
+        <p className='txt1'>make a</p>
+        <Button text='Go-Food' onClick={ undefined }/>
+        <p className='txt1'>order ?</p>
+      </div>
     </div>
   )
 }
